3bd0ad7d06c5bac50402aeaac8c53b5613a79cdf
tabitha12345 <tabby80@hotmail.com>
2025-12-01 11:51:43 -0400

    Added solution for day 1 part 1

---
+++ day01_part1.py
@@ -0,0 +1,41 @@
+# Advent of Code 2025 Day 01 Part 1
+
+input_file = "day01_input.txt"
+
+# Populate a list with the numbers 0-99:
+dial = list(range(100))
+
+# initialize start positions
+dial_location = 50
+counter = 0
+
+try:
+    with open(input_file, 'r') as file:
+        for line in file:
+            # get rid of the new line character
+            line = line.strip()
+            # split the line into the direction and number to spin
+            direction = line[0]
+            number = int(line[1:])
+
+            if direction == "R":
+                # for right turns, add to the number, loop after 99
+                dial_location = (dial_location + number) % 100
+                if dial_location == 0:
+                    # add to the counter if the result is 0
+                    counter = counter + 1
+            elif direction == "L":
+                # for left turns, subtract from the number, loop after 0
+                dial_location = (dial_location - number) % 100
+                if dial_location == 0:
+                    # add to the counter if the result is 0
+                    counter = counter + 1
+
+# account for errors
+except FileNotFoundError:
+    print(f"Error: The file '{input_file}' seems to be missing.")
+except Exception as e:
+    print(f"Another random error occurred: {e}")
+
+print("Part 1: The number of times the counter hits 0 is " + str(counter))
+
